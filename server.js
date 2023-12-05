23253b4ef43c1210807699875bd64c1ec9a27c13
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2023-12-04 19:51:14 -0500

    added port change for production

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cors from "cors"
 
 
 const app = Express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 // Express config below 👇
 const __dirname = path.resolve(path.dirname(''));
